1b5037c3b88072ba81b449e99620d82e6cd27d85
RawanMostafa08 <rawanmostafa401@gmail.com>
2024-11-03 04:52:43 +0200

    feat: Add login, signup components

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,90 @@
+<template>
+
+    <h1 class="title">Login</h1>
+    <form class="login" method="post">
+
+        <div class="form-field">
+            <label for="username">Username</label>
+            <input type="text" id="username" class="username">
+        </div>
+
+        <div class="form-field">
+            <label for="password">Password</label>
+            <input type="password" id="password" class="password">
+        </div>
+
+        <button class="submit-btn" type="submit">Login</button>
+    </form>
+</template>
+
+<script>
+
+</script>
+
+<style>
+html,
+body {
+    height: 100%;
+    margin: 0;
+}
+
+body {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(209, 243, 240, 0.634);
+}
+
+.login {
+    width: fit-content;
+    border: 3px solid rgb(51, 93, 107);
+    border-radius: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(147, 174, 172, 0.634);
+}
+
+.title {
+    color: rgb(36, 73, 70);
+    text-align: center;
+}
+
+.form-field {
+    margin-bottom: 15px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+}
+
+.form-field input {
+    padding: 10px;
+    margin: 20px;
+    font-size: 16px;
+    width: 60%;
+    border-radius: 5px;
+}
+
+.form-field label {
+    margin: 20px;
+    width: 150px;
+}
+
+.submit-btn {
+    padding: 10px 20px;
+    font-size: 16px;
+    margin: 20px;
+    background-color: rgb(51, 93, 107);
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+.submit-btn:hover {
+    background-color: rgb(35, 64, 74);
+}
+</style>
---
+++ frontend/src/components/Register.vue
@@ -0,0 +1,105 @@
+<template>
+
+    <h1 class="title">Sign Up</h1>
+    <form class="signup" method="post">
+        <div class="form-field">
+            <label for="first-name">First Name</label>
+            <input type="text" id="first-name" class="first-name">
+        </div>
+
+        <div class="form-field">
+            <label for="second-name">Second Name</label>
+            <input type="text" id="second-name" class="second-name">
+        </div>
+
+        <div class="form-field">
+            <label for="username">Username</label>
+            <input type="text" id="username" class="username">
+        </div>
+
+        <div class="form-field">
+            <label for="password">Password</label>
+            <input type="password" id="password" class="password">
+        </div>
+
+        <div class="form-field">
+            <label for="conform-password">Confirm password</label>
+            <input type="password" id="conform-password" class="conform-password">
+
+        </div>
+
+        <button class="submit-btn" type="submit">Sign Up</button>
+    </form>
+</template>
+
+<script>
+
+</script>
+
+<style>
+html,
+body {
+    height: 100%;
+    margin: 0;
+}
+
+body {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(209, 243, 240, 0.634);
+}
+
+.signup {
+    width: fit-content;
+    border: 3px solid rgb(51, 93, 107);
+    border-radius: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(147, 174, 172, 0.634);
+}
+
+.title {
+    color: rgb(36, 73, 70);
+    text-align: center;
+}
+
+.form-field {
+    margin-bottom: 15px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+}
+
+.form-field input {
+    padding: 10px;
+    margin: 20px;
+    font-size: 16px;
+    width: 60%;
+    border-radius: 5px;
+}
+
+.form-field label {
+    margin: 20px;
+    width: 150px;
+}
+
+.submit-btn {
+    padding: 10px 20px;
+    font-size: 16px;
+    margin: 20px;
+    background-color: rgb(51, 93, 107);
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+.submit-btn:hover {
+    background-color: rgb(35, 64, 74);
+}
+</style>
